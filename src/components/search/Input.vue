857ec3603cf43deb8fcfb717c94972307e05f9e8
Adam Stępień <vesemir2k@gmail.com>
2024-09-27 19:37:11 +0200

    📝 (Input.vue): improve accessibility by adding aria-label to search button 🐛 (Input.vue): fix getWeather function to handle empty input value and improve error handling for fetching weather data

--- src/components/search/Input.vue
+++ src/components/search/Input.vue
@@ -5,9 +5,14 @@
       class="search-input__input"
       type="text"
       v-model="inputValue"
-      @keydown.enter.prevent
+      @keydown.enter.prevent="getWeather"
+      ref="inputRef"
     />
-    <span @click="getWeather" class="search-input__button">
+    <span
+      @click="getWeather"
+      class="search-input__button"
+      aria-label="Search weather"
+    >
       <v-icon icon="mdi-magnify" size="x-large"></v-icon>
     </span>
   </div>
@@ -23,22 +28,28 @@ const weatherStore = useWeatherStore();
 
 // ~ refs
 const inputValue = ref("");
+const inputRef = ref(null);
 
 // ~ getWeather
-const getWeather = () => {
-  if (weatherStore.placeData) {
-    console.log("Coord");
+const getWeather = async () => {
+  if (!inputValue.value.trim()) {
+    console.log("Please enter a valid city name.");
+    return;
+  }
 
-    console.log(inputValue.value);
-    weatherStore.fetchWeatherCoord(
-      weatherStore.placeData.geometry.location.lat(),
-      weatherStore.placeData.geometry.location.lng()
-    );
-  } else {
-    console.log("City");
+  try {
+    if (weatherStore.placeData?.geometry) {
+      await weatherStore.fetchWeatherCoord(
+        weatherStore.placeData.geometry.location.lat(),
+        weatherStore.placeData.geometry.location.lng()
+      );
+    } else {
+      await weatherStore.fetchWeatherCity(inputValue.value);
+    }
 
-    console.log(inputValue.value);
-    weatherStore.fetchWeatherCity(inputValue.value);
+    inputRef.value.blur();
+  } catch (error) {
+    console.error("Failed to fetch weather data:", error);
   }
 };
 </script>
@@ -48,15 +59,18 @@ const getWeather = () => {
   background-color: rgba(255, 255, 255, 0.4);
   border-radius: 4px;
   position: relative;
+
   &__input {
     width: 100%;
     padding: 8px;
     font-size: 16px;
     border-radius: inherit;
+
     &:focus {
       outline: 2px solid rgba(255, 255, 255, 0.6);
     }
   }
+
   &__button {
     position: absolute;
     top: 0;
